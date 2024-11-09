e173bac8d4134562d829357bf479c0fa754b6801
Ryan Racicot <ryanmracicot@gmail.com>
2024-11-09 15:03:15 -0500

    Customized look of the sliders

--- src/components/TabCard/TabCard.tsx
+++ src/components/TabCard/TabCard.tsx
@@ -7,6 +7,8 @@ import { TabCardProps } from "../../types/TabCardProps"
 import { UpdateEqualizerMessage } from "../../types/messages"
 import { Filters } from "../../types/Filter"
 import styles from "./TabCard.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSync, faTimes } from "@fortawesome/free-solid-svg-icons"
 
 export const TabCard: React.FC<TabCardProps> = ({
     id,
@@ -67,47 +69,63 @@ export const TabCard: React.FC<TabCardProps> = ({
         })
     }
 
+    const focusTab = () => {
+        chrome.tabs.update(id, {
+            active: true,
+        })
+    }
+
     return (
-        <div className={styles.tabCard}>
+        <div className={styles.tabcard}>
             <div className="p-4 bg-white rounded-lg shadow mb-4 hover:shadow-md transition-shadow">
                 <div className="flex justify-between items-start">
-                    <div className="flex-1">
-                        <h3 className="font-medium text-gray-900 truncate">
-                            {title}
-                        </h3>
-                        <p className="text-sm text-gray-500 truncate">{url}</p>
+                    <div className={styles.header}>
+                        <div className={styles.headerText}>
+                            <h3 className={styles.title} onClick={focusTab}>
+                                {title}
+                            </h3>
+                            <p
+                                className={
+                                    styles.url +
+                                    " text-sm text-gray-500 truncate"
+                                }
+                            >
+                                {url}
+                            </p>
+                        </div>
+                        <FontAwesomeIcon
+                            className={styles.closeIcon}
+                            icon={faTimes}
+                            onClick={sendStopRecordingMessage}
+                        />
                     </div>
                     {filters && (
-                        <div className="filters">
+                        <div className={styles.filters}>
                             {Object.entries(filters).map(([id, filter]) => (
-                                <div>
-                                    {filter.frequency} Hz
+                                <div className={styles.filter}>
                                     <input
                                         type="range"
-                                        min="-20"
+                                        min="-40"
                                         max="20"
                                         value={filter.gain}
                                         id={id}
-                                        step="1"
+                                        step=".1"
                                         list="volsettings"
                                         onInput={onSliderUpdate}
+                                        className={styles.inputSlider}
                                     />
+                                    <p className={styles.frequencyLabel}>
+                                        {filter.frequency}
+                                        {filter.frequency < 1000 ? "Hz" : "kHz"}
+                                    </p>
                                 </div>
                             ))}
-                            <div className="flex items-center gap-2">
-                                <button
-                                    onClick={resetFilters}
-                                    className="ml-2 px-2 py-1 text-sm rounded "
-                                >
-                                    Reset
-                                </button>
-                                <button
-                                    onClick={sendStopRecordingMessage}
-                                    className={`ml-2 px-2 py-1 text-sm rounded bg-red-500 text-white hover:bg-red-600`}
-                                >
-                                    Stop
-                                </button>
-                            </div>
+
+                            <FontAwesomeIcon
+                                className={styles.resetIcon}
+                                icon={faSync}
+                                onClick={resetFilters}
+                            />
                         </div>
                     )}
                 </div>
